d2afde5b4ed70d799653224034b0fce401dd7f3f
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-12-07 23:16:41 -0400

    feat: add field validation to sign in form

--- src/app/auth/sign-in/sign-in-form.tsx
+++ src/app/auth/sign-in/sign-in-form.tsx
@@ -7,6 +7,8 @@ import { selfUseFormState } from "@/hooks/self-use-form-state";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { signInfWithEmailAndPasswordAction } from "./actions";
+import { AlertTriangle, Loader2 } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function SignInForm() {
   const router = useRouter();
@@ -18,20 +20,40 @@ export default function SignInForm() {
   return (
     <div className="space-y-4">
       <form onSubmit={handleSubmit} className="space-y-4">
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Falha ao logar!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
+        )}
         <div className="space-y-1">
           <Label htmlFor="email">E-mail</Label>
           <Input id="email" name="email" type="text" />
+
+          {errors?.email && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.email[0]}
+            </p>
+          )}
         </div>
 
         <div className="space-y-1">
           <Label htmlFor="password">Senha</Label>
           <Input id="password" name="password" type="password" />
+          {errors?.password && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.password[0]}
+            </p>
+          )}
 
           <Link href="/auth/forgot-password">Esqueci minha senha</Link>
         </div>
 
         <Button type="submit" className="w-full">
-          Entrar
+          {isPending ? <Loader2 className="size-4 animate-spin" /> : "Entrar"}
         </Button>
 
         <Button
